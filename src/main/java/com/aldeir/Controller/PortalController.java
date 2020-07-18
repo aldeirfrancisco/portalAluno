47ae09a59066b062e5c311bfde05acdbab00594d
aldeir francisco <dide123francisco@gmail.com>
2020-07-17 23:05:06 -0300

    criamos diciplinas

--- src/main/java/com/aldeir/Controller/PortalController.java
+++ src/main/java/com/aldeir/Controller/PortalController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/Aluno")
 public class PortalController {
 	
-	@RequestMapping("/home")
+	
+	@RequestMapping("/Dashboard")
 	public String home() {
 	
 		return "home";
